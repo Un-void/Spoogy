8aebeebb8300f5f30f1fe8cac9ea6855570dc0c9
Un-void <pvnwagh332@gmail.com>
2025-03-06 15:54:13 +0530

    Merged API, Added shimmer effect, Use effect

--- src/components/CardContainer.jsx
+++ src/components/CardContainer.jsx
@@ -2,41 +2,58 @@ import React from "react"
 import Cards from "./Cards"
 import ShimmerCard from "./ShimmerCard"
 import { useState, useEffect } from "react"
-import { Data, API_URL } from "../Constants/config"
+import { IMG_URL, API_URL } from "../Constants/config"
 
-const CardContainer = () =>{
+const CardContainer = () => {
 
-    const [NewData, setNewData] = useState(Data)
+    const [NewData, setNewData] = useState([])
+    const [SearchText, setSearchText] = useState("")
+    const [OriginalData, setOriginalData] = useState([]);
 
-    const getRestaurantData = async() =>{
+    const getRestaurantData = async () => {
         const response = await fetch(API_URL);
         const data = await response.json()
-        console.log("json", data.data.cards[0])
-        console.log("json", data.data.cards[1].card.card.gridElements.infoWithStyle.restaurants)
+        const restaurants = data?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+        setNewData(restaurants)
+        setOriginalData(restaurants)
+        console.log(restaurants[0])
     }
 
-    const handleRating = () =>{
-        const filteredList = Data.filter(restaurant =>
-         restaurant.rating >= 4.8
-     )
-      setNewData(filteredList)
-      console.log("filteredList", filteredList)
+    const handleSearchText = (val) => {
+        setSearchText(val)
     }
 
-    getRestaurantData();
-    return(
-        <>  
-            <button className="ms-48 mt-5 w-36 py-2 px-5 rounded-md bg-gray-400 hover:bg-gray-500" onClick={handleRating}>Top Rated !</button>
+    const handleRating = () => {
+        const filteredList = OriginalData.filter(restaurant =>
+            restaurant.info.avgRating >= 4.5
+        )
+        setNewData(filteredList)
+    }
+
+    useEffect(() => {
+        getRestaurantData();
+        console.log("UseEffect called !")
+    }, [SearchText])
+
+    console.log("Rendered...")
+    return (
+        <>
+            <div className="w-3/6 m-auto flex justify-between items-center pt-5 mt-5">
+                <input type="text" className="border w-2/4 shadow-md h-4/5 p-1 rounded-md" onChange={handleSearchText} />
+                <button className="w-36 px-5 py-2 rounded-md bg-gray-400 hover:bg-gray-500" onClick={handleRating}>Top Rated !</button>
+            </div>
             <div className="m-auto flex gap-6 justify-start w-3/4 mt-16 flex-wrap">
-                {
-                    NewData.map((restaurant)=>{
-                        return <Cards 
-                        {...restaurant }
+                {NewData.length === 0 ?
+                    (<ShimmerCard />)
+                    :
+                    (NewData.map((restaurant) => {
+                        return <Cards
+                            {...restaurant?.info}
                         />
                     })
-                }
+                    )}
             </div>
-            {/* <ShimmerCard/> */}
+
         </>
     )
 }
--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,18 +1,18 @@
+import { IMG_URL } from "../Constants/config"
 
-
-const Cards = ({link, restaurantName, rating, deliveryTime, cusines, location}) =>{
+const Cards = ({cloudinaryImageId, name, avgRating, sla, cuisines, locality}) =>{
     return(
         <>
             <div className="w-56">
-                <img src={link} className="w-56 h-36 object-cover rounded-xl shadow-md"/>
-                <p className="font-extrabold text-xl ms-2">{restaurantName}</p>
+                <img src={IMG_URL + cloudinaryImageId} className="w-56 h-36 object-cover rounded-xl shadow-md"/>
+                <p className="font-extrabold text-xl ms-2">{name}</p>
                 <div className="flex ms-2">
-                    <div className="font-semibold">⭐{rating}</div>
-                    <div className="font-bold ps-2">{deliveryTime}</div>
+                    <div className="font-semibold">⭐{avgRating}</div>
+                    <div className="font-bold ps-2">{sla?.deliveryTime}.mins</div>
                 </div>
                 <div className="text-gray-500 ms-2">
-                    <div >{cusines}</div>
-                    <div>{location}</div>
+                    <div >{cuisines.join(", ")}</div>
+                    <div>{locality}</div>
                 </div>
             </div>
         </>
